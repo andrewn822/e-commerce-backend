b455450e71fcbcf28c4ae052c48f036c08995ea7
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-2.local>
2023-01-03 13:27:21 -0800

    models code

---
+++ Develop/models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Catergory.init(
+    {
+        id: {
+            type: DataTypes.INTERGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+
+        }
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+
+    }
+);
+
+module.exports = Category;
---
+++ Develop/models/Product.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Category',
+                key: 'id',
+                unqieu: false
+            }
+        }
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+    }
+);
+
+module.exports = Product;
